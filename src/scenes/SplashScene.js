17a3c6ce043b6e69915880c5db6a91b45fc4f5fb
Rahul Saliya <RHS.Dev@outlook.com>
2024-01-28 16:01:35 -0400

    - improved game end.

--- src/scenes/SplashScene.js
+++ src/scenes/SplashScene.js
@@ -28,29 +28,25 @@ export default class SplashScene extends Phaser.Scene {
         const centerY = this.cameras.main.height / 2;
 
         // Position splashImage2
-        this.splashImage2 = this.add.sprite(centerX, centerY - 100, 'background-splash2'); // Adjust the vertical position
+        this.splashImage2 = this.add.sprite(centerX, centerY - 60, 'background-splash2'); // Adjust the vertical position
         this.splashImage2.setOrigin(0.5, 1); // Set origin to bottom center
-        this.splashImage2.setScale(0.75, 0.6).setScrollFactor(0);
+        this.splashImage2.setScale(0.6).setScrollFactor(0);
         // Position title at the center of the screen
-        const title = this.add.image(centerX - 10, centerY, 'title'); // Center horizontally and vertically
+        const title = this.add.image(centerX, centerY, 'title'); // Center horizontally and vertically
         title.setOrigin(0.5); // Set origin to center
         // Set the scale and scroll factor for title
         title.setScale(1).setScrollFactor(0);
 
-        const startButton = this.add.text(0, 0, '', {
-            fontFamily: 'Caramel',
-            fontSize: `0px`,
-        });
-
-        const loadingText = this.add.text(centerX, title.y + title.displayHeight + 10, 'Loading....', {
+        const style =
+        {
             fontSize: '45px',
             color: '#ffffff',
             fontFamily: 'Caramel',
-        });
+        };
 
+        const loadingText = this.add.text(centerX, title.y + title.displayHeight + 10, 'Loading....', style);
         loadingText.setOrigin(0.5); // Set origin to center
         loadingText.setScrollFactor(0);
-
     }
 
     update(time, diff) {
